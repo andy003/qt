225b3bd5ec02f10a7fc9abbb6a54c01fad02599f
zhangran <28281539@qq.com>
2020-01-13 14:49:01 +0800

    SYNC

--- MediaPlayer/main.cpp
+++ MediaPlayer/main.cpp
@@ -12,11 +12,11 @@
 
 #include "media_player.h"
 #include <QtWidgets/QApplication>
-//
-//int main(int argc, char* argv[])
-//{
-//  QApplication a(argc, argv);
-//  MediaPlayer w;
-//  w.show();
-//  return a.exec();
-//}
+
+int main(int argc, char* argv[])
+{
+  QApplication a(argc, argv);
+  MediaPlayer w;
+  w.show();
+  return a.exec();
+}
--- MediaPlayer/rtsp_client.h
+++ MediaPlayer/rtsp_client.h
@@ -33,16 +33,32 @@ protected:
   void mousePressEvent(QMouseEvent* event) override;
 };
 
+/**
+ *
+ */
+class RTSPException : public std::logic_error
+{
+public:
+  RTSPException(const std::string& what) : std::logic_error(what)
+  {
+  }
+};
+
 /*
  *----------------------------------------------------------------------------------------------------------------------
- *                                                          
+ *
  *----------------------------------------------------------------------------------------------------------------------
  */
 class StreamClientState
 {
 public:
-  StreamClientState();
-  virtual ~StreamClientState();
+  StreamClientState() : iter(nullptr), session(nullptr), subsession(nullptr), streamTimerTask(nullptr), duration(0.0)
+  {
+  }
+
+  virtual ~StreamClientState()
+  {
+  }
 
 public:
   MediaSubsessionIterator* iter;
@@ -54,24 +70,96 @@ public:
 
 /*
  *----------------------------------------------------------------------------------------------------------------------
- *                                                          
+ *
  *----------------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * 虚拟槽，用来接收 RTSP 数据
+ */
+class DummySink : public MediaSink
+{
+private:
+  u_int8_t* fReceiveBuffer;
+  MediaSubsession& fSubsession;
+  char* fStreamId;
+
+public:
+  static DummySink* createNew(UsageEnvironment& env, MediaSubsession& subsession, char const* streamId = NULL);
+
+private:
+  /**
+   * Ctor
+   */
+  DummySink(UsageEnvironment& env, MediaSubsession& subsession, char const* streamId);
+
+  /**
+   * Dtor
+   */
+  virtual ~DummySink();
+
+  /**
+   * Dtor
+   */
+  static void afterGettingFrame(void* clientData, unsigned frameSize, unsigned numTruncatedBytes,
+                                struct timeval presentationTime, unsigned durationInMicroseconds);
+
+  /**
+   * Dtor
+   */
+  void afterGettingFrame(unsigned frameSize, unsigned numTruncatedBytes, struct timeval presentationTime,
+                         unsigned durationInMicroseconds);
+
+private:
+  // redefined virtual functions:
+  virtual Boolean continuePlaying();
+};
+
+/*
+ *----------------------------------------------------------------------------------------------------------------------
+ *
+ *----------------------------------------------------------------------------------------------------------------------
+ */
+/**
+ * RTSP 客户端
  */
 class MyRTSPClient : public RTSPClient
 {
-public:
-  static MyRTSPClient* create(UsageEnvironment& env, char const* rtspURL, int verbosityLevel = 0,
-                                 char const* applicationName = NULL, portNumBits tunnelOverHTTPPortNum = 0);
+private:
+  StreamClientState scs;
 
-protected:
+public:
+  /**
+   * Ctor
+   */
   MyRTSPClient(UsageEnvironment& env, char const* rtspURL, int verbosityLevel, char const* applicationName,
                portNumBits tunnelOverHTTPPortNum);
 
-  // called only by createNew();
+  /**
+   * Dtor
+   */
   virtual ~MyRTSPClient();
 
-public:
-  StreamClientState scs;
+  /**
+   * 打开 URL
+   */
+  void openURL(UsageEnvironment& env, char const* progName, char const* rtspURL);
+
+private:
+  void shutdownStream();
+  void initMediaSession(const std::string& description);
+  void setupAllSessions();
+
+  void afterDescribeHandler(int resultCode, char* resultString);
+
+  static inline void afterDescribeHandlerWrapper(RTSPClient* rtspClient, int resultCode, char* resultString);
+
+  friend UsageEnvironment& operator<<(UsageEnvironment& env, const RTSPClient& rtspClient);
 };
+
+inline void MyRTSPClient::afterDescribeHandlerWrapper(RTSPClient* rtspClient, int resultCode, char* resultString)
+{
+  static_cast<MyRTSPClient*>(rtspClient)->afterDescribeHandler(resultCode, resultString);
+}
 
 #endif
